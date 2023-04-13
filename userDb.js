d6e271035f8c24fe9d937a902878e8eb5f025ccf
Inderjeet <inderjeet@signitysolutions.in>
2023-04-13 15:51:13 +0530

    first commit

--- userDb.js
+++ userDb.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Admin', 'root', 'Toor!!!123', {
+const sequelize = new Sequelize('Admin', 'signity', 'root', {
   host: 'localhost',
   dialect: 'mysql'
 });
